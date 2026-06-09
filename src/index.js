244881b54393d0b6679cf919a8dae079f8a9d38a
enzie <mulyaenziepratama@gmail.com>
2026-06-09 18:16:32 +0700

    add entry point the app at index.js

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const jobsRoute = require('./route/job');
+const { connectDB } = require('./db/db');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from client folder
+app.use(express.static(path.join(__dirname, '../client')));
+
+// Routes
+app.use('/api', jobsRoute);
+
+// Start server
+const start = async () => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  });
+};
+
+start();
